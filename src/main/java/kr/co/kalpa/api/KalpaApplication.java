c04947986a1cc8e1e260946a4dacfbd6b303e7df
KimDoYoung <kdy987@gmail.com>
2026-02-23 15:48:52 +0900

    war만들

--- src/main/java/kr/co/kalpa/api/KalpaApplication.java
+++ src/main/java/kr/co/kalpa/api/KalpaApplication.java
@@ -2,11 +2,18 @@ package kr.co.kalpa.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = "kr.co.kalpa")
 @EnableJpaAuditing
-public class KalpaApplication {
+public class KalpaApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KalpaApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KalpaApplication.class, args);
